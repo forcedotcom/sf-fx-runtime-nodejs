1437edec988d343fe35a11858d3cfa7514bb540a
Lillian Zhang <zhang.l@salesforce.com>
2021-04-20 13:51:32 -0700

    createOrg method

--- src/sdk/org.ts
+++ src/sdk/org.ts
@@ -3,6 +3,15 @@ import {
   SalesforceFunctionContextCloudEventExtension,
 } from "../extensions";
 
+export function createOrg(
+  contextExt: SalesforceContextCloudEventExtension,
+  functionContextExt: SalesforceFunctionContextCloudEventExtension
+): Org {
+  let org = new Org(contextExt, functionContextExt);
+  
+  return org;
+}
+
 export class Org {
   id: string;
   baseUrl: string;
@@ -12,11 +21,15 @@ export class Org {
   user: User;
 
   constructor(contextExt: SalesforceContextCloudEventExtension, functionContextExt: SalesforceFunctionContextCloudEventExtension) {
+<<<<<<< Updated upstream
     this.id = contextExt.userContext.orgId;
     this.baseUrl = contextExt.userContext.salesforceBaseUrl;
     this.domainUrl = contextExt.userContext.orgDomainUrl;
     this.apiVersion = contextExt.apiVersion;
     this.dataApi = new DataApi(this.baseUrl, this.apiVersion, this.accessToken);
     this.user = new User(contextExt.userContext.userId, contextExt.userContext.username, contextExt.userContext.onBehalfOfUserId);
+=======
+
+>>>>>>> Stashed changes
   }
 }
