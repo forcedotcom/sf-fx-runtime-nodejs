db042eea579f5a5c3a4845649028baa301bd92dc
Danielle Adams <danielle.adams@salesforce.com>
2021-05-25 17:05:17 -0400

    Add if statement to look for default-namespaced functions and move the extracting of user function into its own function for better testing

--- src/user-function.ts
+++ src/user-function.ts
@@ -1,6 +1,37 @@
 import * as path from "path";
 import { SalesforceFunction } from "./sdk-interface-v1";
 
+export function loadDefaultExport(
+  directory: string,
+  main: string
+): SalesforceFunction<unknown, unknown> {
+  // Load default export of module defined in 'main' field of 'package.json'
+  let defaultExport;
+  try {
+    defaultExport = require(path.join(directory, main));
+
+    // Allows to find default export from TypeScript builds
+    if (defaultExport.default && defaultExport.default instanceof Function)
+      defaultExport = defaultExport.default;
+  } catch (error) {
+    if (error.code === "MODULE_NOT_FOUND") {
+      throw new Error(
+        "Could not load module referenced in 'main' field of 'package.json': " +
+          error.message
+      );
+    }
+  }
+
+  // Validate default export
+  if (!(defaultExport instanceof Function)) {
+    throw new Error(
+      "Default export of module referenced in 'main' field of 'package.json' is not a function!"
+    );
+  }
+
+  return defaultExport;
+}
+
 export function loadUserFunctionFromDirectory(
   directory: string
 ): SalesforceFunction<unknown, unknown> {
@@ -25,25 +56,5 @@ export function loadUserFunctionFromDirectory(
     );
   }
 
-  // Load default export of module defined in 'main' field of 'package.json'
-  let defaultExport;
-  try {
-    defaultExport = require(path.join(directory, packageJson.main));
-  } catch (error) {
-    if (error.code === "MODULE_NOT_FOUND") {
-      throw new Error(
-        "Could not load module referenced in 'main' field of 'package.json': " +
-          error.message
-      );
-    }
-  }
-
-  // Validate default export
-  if (!(defaultExport instanceof Function)) {
-    throw new Error(
-      "Default export of module referenced in 'main' field of 'package.json' is not a function!"
-    );
-  }
-
-  return defaultExport;
+  return loadDefaultExport(directory, packageJson.main);
 }
