ca4111355218e561bcb5ea0c9e30d55e5f2096af
Lillian Zhang <zhang.l@salesforce.com>
2021-05-27 13:47:53 -0700

    run prettier

--- test/cloud-event.ts
+++ test/cloud-event.ts
@@ -1,15 +1,22 @@
 import { expect } from "chai";
-import { parseCloudEvent, SalesforceFunctionsCloudEvent} from "../src/cloud-event";
+import {
+  parseCloudEvent,
+  SalesforceFunctionsCloudEvent,
+} from "../src/cloud-event";
 import * as CloudEvents from "cloudevents";
 
 describe("parseCloudEvent", () => {
-  const headers = {content_type: "something", blah: "blah blah"};
+  const headers = { content_type: "something", blah: "blah blah" };
   const body = "";
   it("returns the expected objects", () => {
     const result = parseCloudEvent(headers, body);
     const cloudEvent = CloudEvents.HTTP.toEvent({ headers, body });
-    const sfContext = JSON.parse(Buffer.from(cloudEvent.sfcontext.toString(), "base64").toString("utf-8"));
-    const sfFunctionContext = JSON.parse(Buffer.from(cloudEvent.sffncontext.toString(), "base64").toString("utf-8"))
+    const sfContext = JSON.parse(
+      Buffer.from(cloudEvent.sfcontext.toString(), "base64").toString("utf-8")
+    );
+    const sfFunctionContext = JSON.parse(
+      Buffer.from(cloudEvent.sffncontext.toString(), "base64").toString("utf-8")
+    );
 
     expect(result.cloudEvent).to.equal(cloudEvent);
     expect(result.sfContext).to.equal(sfContext);
@@ -20,11 +27,11 @@ describe("parseCloudEvent", () => {
     function Spy(obj, method) {
       let spy = {
         args: [],
-        count: 0
+        count: 0,
       };
 
       let original = obj[method];
-      obj[method] = function() {
+      obj[method] = function () {
         let args = [].slice.apply(arguments);
         spy.count = spy.count + 1;
         spy.args.push(args);
@@ -32,10 +39,10 @@ describe("parseCloudEvent", () => {
       };
 
       return Object.freeze(spy);
-    };
+    }
 
-    const spy = Spy(CloudEvents.HTTP, 'toEvent');
-    CloudEvents.HTTP.toEvent({ headers, body })
+    const spy = Spy(CloudEvents.HTTP, "toEvent");
+    CloudEvents.HTTP.toEvent({ headers, body });
     expect(spy.count).to.equal(1);
   });
 });
