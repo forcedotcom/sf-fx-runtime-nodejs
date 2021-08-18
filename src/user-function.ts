44359c3bc5a024bcee75dae61f70dedd62408702
Josh W Lewis <josh.lewis@salesforce.com>
2021-08-18 11:28:50 -0500

    Rework user-function tests and fixtures.
    Node relies on this project's package.json module mode to determine what module
    system `.js`  files use, when a `.js` file has no `package.json` of it's
    own. Now that this project is in ESM module mode, it assumes all the
    fixtures are also ESM. We need each CJS fixture to have a package.json
    so we properly detect the fixture's module mode.

--- src/user-function.ts
+++ src/user-function.ts
@@ -1,4 +1,4 @@
-import * as fs from "fs";
+import { promises as fs } from "fs";
 import * as path from "path";
 import { SalesforceFunction } from "sf-fx-sdk-nodejs";
 
@@ -7,43 +7,41 @@ export async function loadDefaultExport(
   main: string
 ): Promise<SalesforceFunction<unknown, unknown>> {
   // Load default export of module defined in 'main' field of 'package.json'
-  let defaultExport;
+  let fExports: any;
+
   try {
-    defaultExport = await import(path.join(directory, main));
-
-    // Allows to find default export from TypeScript builds
-    if (defaultExport.default && defaultExport.default instanceof Function)
-      defaultExport = defaultExport.default;
+    fExports = await import(path.join(directory, main));
   } catch (error) {
-    if (error.code === "MODULE_NOT_FOUND") {
-      throw new Error(
-        "Could not load module referenced in 'main' field of 'package.json': " +
-          error.message
-      );
-    } else {
-      throw error;
-    }
-  }
-
-  // Validate default export
-  if (!(defaultExport instanceof Function)) {
     throw new Error(
-      "Default export of module referenced in 'main' field of 'package.json' is not a function!"
+      "Could not load module referenced in 'main' field of 'package.json': " +
+        error.message
     );
   }
 
-  return defaultExport;
+  if (fExports.default && fExports.default.default && fExports.default.default instanceof Function) {
+    // CJS TypeScript exports
+    return fExports.default.default;
+  } else if (fExports.default && fExports.default instanceof Function) {
+    // CJS JavaScript exports
+    return fExports.default;
+  } else if (fExports instanceof Function) {
+    // ESM exports
+    return fExports;
+  }
+  throw new Error(
+    "Default export of module referenced in 'main' field of 'package.json' is not a function!"
+  );
 }
 
 export async function loadUserFunctionFromDirectory(
   directory: string
 ): Promise<SalesforceFunction<unknown, unknown>> {
-  const packageJsonPath = path.join(directory, "package.json");
+  const packageJsonPath = path.resolve(directory, "package.json");
 
   // Load package.json
   let packageFile;
   try {
-    packageFile = await fs.readFileSync(packageJsonPath);
+    packageFile = await fs.readFile(packageJsonPath, "utf8");
   } catch (error) {
     throw new Error(
       "Could not read 'package.json' from project directory: " + error.message
@@ -65,6 +63,17 @@ export async function loadUserFunctionFromDirectory(
     throw new Error(
       "Could not read 'main' field from 'package.json' in project directory!"
     );
+  }
+
+
+  const functionPath = path.join(directory, packageJson.main)
+  // Verify existence of 'main' file
+  try {
+    await fs.access(functionPath)
+  } catch(err) {
+    throw new Error(
+      `Could not open function file specified by 'main' field from 'package.json' (${functionPath})!`
+    )
   }
 
   return await loadDefaultExport(directory, packageJson.main);
