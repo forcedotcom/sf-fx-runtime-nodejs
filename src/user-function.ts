9bd5896d925cc51ceced9f94702dcc4d0df20969
Lillian Zhang <47681745+lillianzhang331@users.noreply.github.com>
2021-09-01 11:36:28 -0700

    ignore formatting for license header due to newline (#131)

--- src/user-function.ts
+++ src/user-function.ts
@@ -1,3 +1,10 @@
+/*
+ * Copyright (c) 2021, salesforce.com, inc.
+ * All rights reserved.
+ * SPDX-License-Identifier: BSD-3-Clause
+ * For full license text, see the LICENSE file in the repo root or https://opensource.org/licenses/BSD-3-Clause
+ */
+
 import { promises as fs } from "fs";
 import * as path from "path";
 import { SalesforceFunction } from "sf-fx-sdk-nodejs";
