bb29d5e72ac615d9abef31ce55eca52d98497513
Doris Kwan <doris.kwan@salesforce.com>
2022-07-13 14:12:05 -0400

    Update publish script and remove the usage of the S3 bucket (#362)
    * Update publish script and remove the usage of the S3 bucket
    
    * CHANGELOG
    
    * Update readme to remove s3 mentions
    
    * Update publish.sh script

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -5,6 +5,7 @@ The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/),
 and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0.html).
 
 ## [Unreleased]
+- Update publish.sh 
 
 ## [0.11.2] - 2022-06-29
 - Update to enable Salesforce API version v55.0
--- etc/publish.sh
+++ etc/publish.sh
@@ -1,47 +1,9 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-bucket_name=${1:-"sf-fx-nodejs-internal-early-access"}
-aws_cli_profile=${2:-"sf-fx-ea"}
-
-# The package filename cannot be customized for npm pack.
-# We need to figure out the file it will write to on our own:
-name=$(jq -r '.name | gsub("@";"") | gsub("/";"-")' package.json)
-version=$(jq -r .version package.json)
-package_filename="${name}-${version}.tgz"
-
-if [[ -f "${package_filename}" ]]; then
-	echo "Packaged tarball already exists, aborting..."
-	exit 1
-fi
-
-if aws s3api head-object --bucket "${bucket_name}" --key "${package_filename}" --profile "${aws_cli_profile}" > /dev/null; then
-	echo "$(tput setaf 1)${package_filename} already exists in bucket ${bucket_name}!$(tput sgr0)"
-	read -r -p "Overwriting the file might have serious consequences, do you want to continue anyway? [y/N] " continue_response
-	case "${continue_response}" in
-	  [yY])
-		;;
-	  *)
-		exit 0
-		;;
-	esac
-fi
-
-echo "Building package..."
-npm run build
-
-echo "Running npm pack..."
-npm pack
-
-echo "Pushing packaged tarball to s3 bucket:"
-aws s3 cp "${package_filename}" "s3://${bucket_name}" --profile "${aws_cli_profile}"
-
 echo "Running npm publish..."
 npm publish
 
 echo "Creating git tag..."
 git tag "v${version}"
 git push --tags
-
-echo "Cleaning up..."
-rm "${package_filename}"
